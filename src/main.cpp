854a3d239d4ba9c252caf6483311f2cb05ecd759
Leah <leahnyx@proton.me>
2025-10-21 11:11:50 -0600

    + Call HandleArguments from main, though it does nothing at the moment.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <string>
 #include <vector>
 
-#include <portal-cli/player.h>
+#include <portal-cli/argument-handler.h>
 
 int main(int argc, char* argv[])
 {
 
     std::vector<std::string> arguments(argv, argv + argc);
-
-    
+    PortalCli::ArgumentHandler::HandleArguments(arguments);
 
     return 0;
 }
